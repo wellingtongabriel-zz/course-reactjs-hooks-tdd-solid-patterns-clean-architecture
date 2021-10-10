ccf7566e185c4305841c425640be9b5dba22ac02
Wellington Gabriel Barea dos Santos <wellington.santos@picpay.com>
2021-10-10 18:28:51 -0300

    test: ensure axios http client calls axios with correct verb

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -11,10 +11,10 @@ const makeSut = (): AxiosHttpClient => {
 
 describe("AxiosHttpClient", () => {
     
-    it("should call axis  with correct URL", async () => {
+    it("should call axis  with correct URL and verb", async () => {
         const url = faker.internet.url();
         const sut = makeSut();
         await sut.post({ url })
-        expect(mockedAxios).toHaveBeenCalledWith(url);
+        expect(mockedAxios.post).toHaveBeenCalledWith(url);
     });
 });
