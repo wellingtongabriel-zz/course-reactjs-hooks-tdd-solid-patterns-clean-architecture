9d49d7350c1f75afdfaebbbd9e52b72dc0272423
Wellington Gabriel Barea dos Santos <wellington.santos@picpay.com>
2021-10-14 19:01:55 -0300

    feat: refactory spinner component

--- src/presentation/components/spinner/spinner.tsx
+++ src/presentation/components/spinner/spinner.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Styles from './spinner-styles.scss'
 
-const Spinner: React.FC = (props: React.HTMLAttributes<HTMLElement>) => {
+const Spinner: React.FC<{className: string }> = (props: React.HTMLAttributes<HTMLElement>) => {
     return (
         <div {...props} className={[Styles.spinner, props.className].join(' ')}></div>
     )
