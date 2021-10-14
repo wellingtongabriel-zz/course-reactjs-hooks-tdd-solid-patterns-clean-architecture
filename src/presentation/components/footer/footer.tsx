5fa2c1ecb7ef820d0cb906099f743d5e5c5fc0a0
Wellington Gabriel Barea dos Santos <wellington.santos@picpay.com>
2021-10-14 19:51:05 -0300

    refactor: move header and footer to separate files

---
+++ src/presentation/components/footer/footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Styles from './footer-style.scss'
+
+const Footer: React.FC = () => {
+    return (
+        <footer className={Styles.footer} />
+    )
+}
+
+export default Footer
---
+++ src/presentation/components/login-header/login-header.tsx
@@ -0,0 +1,14 @@
+import React, { memo } from 'react'
+import Styles from './login-header-style.scss'
+import Logo from '../logo/logo'
+
+const LoginHeader: React.FC = () => {
+    return (
+        <header className={Styles.header}>
+            <Logo />
+            <h1>  4DEV </h1>
+        </header>
+    )
+}
+
+export default memo(LoginHeader)
