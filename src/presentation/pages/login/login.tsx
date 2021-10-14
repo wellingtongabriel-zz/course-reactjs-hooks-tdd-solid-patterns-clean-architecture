c6ef123403cda19fd8ab4e3f92ff60a50ceb660d
Wellington Gabriel Barea dos Santos <wellington.santos@picpay.com>
2021-10-14 20:02:55 -0300

    refactor: move to formstatus

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,26 +1,23 @@
 import React from 'react'
 import Styles from './login-style.scss'
 
-import Spinner from '@/presentation/components/spinner/spinner'
 import Header from '@/presentation/components/login-header/login-header'
 import Footer from '@/presentation/components/footer/footer'
 import Input from '@/presentation/components/input/input'
+import FormStatus from '@/presentation/components/form-status/form-status'
 
 const Login: React.FC = () => {
     return (
         <div className={Styles.login}>
             <Header />
-            <form className={Styles.form}>
-                <h2> Login </h2>
-                <Input type="email" name="email" placeholder="Digite seu e-mail" />
-                <Input type="password" name="password" placeholder="Digite sea senha" />
-                <button className={Styles.submit} type="submit"> Entrar </button>
-                <span className={Styles.link}> Criar conta </span>
-                <div className={Styles.errorWrap}>
-                    <Spinner className={Styles.spinner} />
-                    <span className={Styles.error}>Erro</span>
-                </div>
-            </form>
+                <form className={Styles.form}>
+                    <h2> Login </h2>
+                    <Input type="email" name="email" placeholder="Digite seu e-mail" />
+                    <Input type="password" name="password" placeholder="Digite sea senha" />
+                    <button className={Styles.submit} type="submit"> Entrar </button>
+                    <span className={Styles.link}> Criar conta </span>
+                    <FormStatus />
+                </form>
             <Footer />
         </div>
     )
