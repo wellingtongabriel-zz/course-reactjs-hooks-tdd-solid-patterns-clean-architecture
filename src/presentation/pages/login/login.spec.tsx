0718f703e0abe3eab6aab7e44df5079a9c88770f
Wellington Gabriel Barea dos Santos <wellington.santos@picpay.com>
2021-10-15 08:35:57 -0300

    test: ensure login start with inputs with required state

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -8,8 +8,16 @@ describe('Login Component', () => {
         
         const errorWrap = getByTestId('error-wrap')
         const submitButton = getByTestId('submit') as HTMLButtonElement
+        const emailStatus = getByTestId('email-status')
+        const passwordStatus = getByTestId('password-status')
         
         expect(errorWrap.childElementCount).toBe(0)
         expect(submitButton.disabled).toBe(true)
+
+        expect(emailStatus.title).toBe('Campo obrigatorio')
+        expect(passwordStatus.title).toBe('Campo obrigatorio')
+        
+        expect(emailStatus.textContent).toBe('🔴')
+        expect(passwordStatus.textContent).toBe('🔴')
     })
 })
