81c24df9661a40ae576b7d535867096afa501b07
Wellington Gabriel Barea dos Santos <wellington.santos@picpay.com>
2021-10-16 15:58:40 -0300

    test: ensure login present error if pasword is invalid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import { render, RenderResult, fireEvent, cleanup } from '@testing-library/react'
-import { ValidationSpy } from '@/presentation/test'
+import { ValidationStub } from '@/presentation/test'
 import faker from 'faker'
 
 import Login from './login'
 
 type SutTypes = {
     sut: RenderResult
-    validationSpy: ValidationSpy
+    validationStub: ValidationStub
 }
 
 const makeSut = (): SutTypes => {
-    const validationSpy = new ValidationSpy()
-    validationSpy.errorMessage = faker.random.words()
-    const sut = render(<Login validation={validationSpy} />)
-    return { sut, validationSpy }
+    const validationStub = new ValidationStub()
+    validationStub.errorMessage = faker.random.words()
+    const sut = render(<Login validation={validationStub} />)
+    return { sut, validationStub }
 }
 
 describe('Login Component', () => {
@@ -22,7 +22,7 @@ describe('Login Component', () => {
     afterEach(cleanup)
 
     it('Should start with initial state', () => {
-        const { sut, validationSpy } = makeSut()
+        const { sut, validationStub } = makeSut()
         
         const errorWrap = sut.getByTestId('error-wrap')
         const submitButton = sut.getByTestId('submit') as HTMLButtonElement
@@ -32,39 +32,17 @@ describe('Login Component', () => {
         expect(errorWrap.childElementCount).toBe(0)
         expect(submitButton.disabled).toBe(true)
 
-        expect(emailStatus.title).toBe(validationSpy.errorMessage)
-        expect(passwordStatus.title).toBe('Campo obrigatorio')
+        expect(emailStatus.title).toBe(validationStub.errorMessage)
+        expect(passwordStatus.title).toBe(validationStub.errorMessage)
         
         expect(emailStatus.textContent).toBe('🔴')
         expect(passwordStatus.textContent).toBe('🔴')
     })
 
-    it('Should call validation with correct email', () => {
-        const { sut, validationSpy } = makeSut()
-        const emailInput = sut.getByTestId('email')
-        const email = faker.internet.email()
-
-        fireEvent.input(emailInput, { target: { value: email } })
-        
-        expect(validationSpy.fieldName).toEqual('email')
-        expect(validationSpy.filedValue).toEqual(email)
-    })
-
-    it('Should call validation with correct password', () => {
-        const { sut, validationSpy } = makeSut()
-        const passwordInput = sut.getByTestId('password')
-        const password = faker.internet.password()
-
-        fireEvent.input(passwordInput, { target: { value: password } })
-        
-        expect(validationSpy.fieldName).toEqual('password')
-        expect(validationSpy.filedValue).toEqual(password)
-    })
-
     it('Should show email error is validation fails', () => {
-        const { sut, validationSpy } = makeSut()
+        const { sut, validationStub } = makeSut()
         const errorMessage = faker.random.words()
-        validationSpy.errorMessage = errorMessage
+        validationStub.errorMessage = errorMessage
         const emailInput = sut.getByTestId('email')
 
         fireEvent.input(emailInput, { target: { value: faker.internet.email() } })
@@ -72,5 +50,18 @@ describe('Login Component', () => {
 
         expect(emailStatus.title).toEqual(errorMessage)
         expect(emailStatus.textContent).toEqual('🔴')
+    })
+
+    it('Should show password error is validation fails', () => {
+        const { sut, validationStub } = makeSut()
+        const errorMessage = faker.random.words()
+        validationStub.errorMessage = errorMessage
+        const passwordInput = sut.getByTestId('password')
+
+        fireEvent.input(passwordInput, { target: { value: faker.internet.password() } })
+        const passwordStatus = sut.getByTestId('password-status')
+
+        expect(passwordStatus.title).toEqual(errorMessage)
+        expect(passwordStatus.textContent).toEqual('🔴')
     })
 })
