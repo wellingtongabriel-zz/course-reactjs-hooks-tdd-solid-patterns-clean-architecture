917ffa7249028e55f824ec51fd2aba506b0d844d
Wellington Gabriel Barea dos Santos <wellington.santos@picpay.com>
2021-10-27 20:27:46 -0300

    feat: ensure required field validation returns error if field is empty

---
+++ src/validation/required-field/required-filed-validation.ts
@@ -0,0 +1,11 @@
+import { RequiredFieldError } from '../errors'
+import { FieldValidation } from '../protocols/field-validation'
+
+export class RequiredFieldValidation implements FieldValidation {
+    
+    constructor(readonly fieldName: string) {}
+
+    validate (value: string): Error {
+        return new RequiredFieldError()
+    }
+}
