37ac36ac13e25b81d585af94e84ea2bb8aac6686
Wellington Gabriel Barea dos Santos <wellington.santos@picpay.com>
2021-10-27 20:30:34 -0300

    test: ensure required field validation returns falsy if field is not empty

--- src/validation/required-field/required-filed.validation.spec.tsx
+++ src/validation/required-field/required-filed.validation.spec.tsx
@@ -1,10 +1,18 @@
 import { RequiredFieldError } from "../errors"
 import { RequiredFieldValidation } from "./required-filed-validation"
+import faker from 'faker'
 
 describe('RequiredFieldValidation', () => {
+ 
     it('Should return error if field is empty', () => {
         const sut = new RequiredFieldValidation('email')
         const error = sut.validate('')
         expect(error).toEqual(new RequiredFieldError())
+    })
+
+    it('Should return falsy if field is not empty', () => {
+        const sut = new RequiredFieldValidation('email')
+        const error = sut.validate(faker.random.word())
+        expect(error).toBeFalsy()
     })
 })
