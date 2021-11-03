4b0c5d8fa93b0d7cdcde8be355c9ae2179cce987
Wellington Gabriel Barea dos Santos <wellington.santos@picpay.com>
2021-11-03 20:23:47 -0300

    test: ensure min length validation returns falsy if value is valid

--- src/validation/validators/min-length/min-lenght-validation.spec.ts
+++ src/validation/validators/min-length/min-lenght-validation.spec.ts
@@ -8,4 +8,10 @@ describe('MinLenghtValidation', () => {
         const error = sut.validate('123')
         expect(error).toEqual(new InvalidFieldError())
     })
+
+    it('Should return false if valie is valid', () => {
+        const sut = new MinLengthValidation('field', 5)
+        const error = sut.validate('12345')
+        expect(error).toBeFalsy()
+    })
 })
