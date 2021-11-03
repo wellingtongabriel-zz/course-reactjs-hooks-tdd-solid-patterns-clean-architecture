c88bd50fb31c4505a473fa70a665c79531c142d0
Wellington Gabriel Barea dos Santos <wellington.santos@picpay.com>
2021-11-03 20:31:43 -0300

    refactor: move SUT creation to a factory method

--- src/validation/validators/min-length/min-lenght-validation.spec.ts
+++ src/validation/validators/min-length/min-lenght-validation.spec.ts
@@ -1,17 +1,20 @@
 import { InvalidFieldError } from '../../errors'
 import { MinLengthValidation } from './min-lenght-validation'
+import faker from 'faker'
+
+const makeSut = (): MinLengthValidation => new MinLengthValidation(faker.database.column(), 5)
 
 describe('MinLenghtValidation', () => {
     
     it('Should return error if valie is invalid', () => {
-        const sut = new MinLengthValidation('field', 5)
-        const error = sut.validate('123')
+        const sut = makeSut()
+        const error = sut.validate(faker.random.alphaNumeric(4))
         expect(error).toEqual(new InvalidFieldError())
     })
 
     it('Should return false if valie is valid', () => {
-        const sut = new MinLengthValidation('field', 5)
-        const error = sut.validate('12345')
+        const sut = makeSut()
+        const error = sut.validate(faker.random.alphaNumeric(5))
         expect(error).toBeFalsy()
     })
 })
