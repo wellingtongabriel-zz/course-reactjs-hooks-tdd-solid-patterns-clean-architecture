f8781ef97b241ddff19b8f661f83c16420065967
Wellington Gabriel Barea dos Santos <wellington.santos@picpay.com>
2021-11-06 16:39:03 -0300

    test: ensure validation builder has a email helper

--- src/validation/builder/validation-builder.spec.ts
+++ src/validation/builder/validation-builder.spec.ts
@@ -1,9 +1,14 @@
-import { RequiredFieldValidation } from '../validators/required-field/required-filed-validation'
+import { RequiredFieldValidation, EmailValidation } from '../validators'
 import { ValidationBuilder as sut } from './validation-builder'
 
 describe('ValidationBuilder', () => {
     it('Should return requiredFieldValidation', () => {
         const validations = sut.field('any_field').required().build()
         expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+    })
+
+    it('Should return emailValidation', () => {
+        const validations = sut.field('any_email').email().build()
+        expect(validations).toEqual([new EmailValidation('any_email')])
     })
 })
